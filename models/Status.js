738bc857e9e54b42117fa8a9de8466cce9d20321
popetrov <popetrov64@gmail.com>
2022-10-05 14:54:24 +0400

    delete all

--- models/Status.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, Types } = require('mongoose');
-
-const schema = new Schema({
-	owner: { type: Types.ObjectId, ref: 'User' },
-	data: { emoji: String, status: String },
-});
-
-module.exports = model('Status', schema);
--- models/User.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model, Types } = require('mongoose');
-
-const schema = new Schema({
-	email: { type: String, unique: true, required: true },
-	password: { type: String, required: true },
-	status: [{ type: Types.ObjectId, ref: 'Status' }],
-});
-
-module.exports = model('User', schema);
