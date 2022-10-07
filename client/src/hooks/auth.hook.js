53ba77db61dc82aa96cee328f83128c5a18e575d
popetrov <popetrov64@gmail.com>
2022-10-07 16:19:30 +0400

    added context and auth hook

---
+++ client/src/hooks/auth.hook.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export const useAuth = () => {
+	const [token, setToken] = useState(null);
+	const [userId, setUserId] = useState(null);
+	const [isReady, setIsReady] = useState(false);
+
+	const login = (jwtToken, id) => {
+		setToken(jwtToken);
+		setUserId(id);
+		localStorage.setItem(
+			'userData',
+			JSON.stringify({
+				userId: id,
+				token: jwtToken,
+			})
+		);
+	};
+
+	const logout = () => {
+		setToken(null);
+		setUserId(null);
+		localStorage.removeItem('userData');
+	};
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem('userData'));
+		if (data && data.token) {
+			login(data.token, data.userId);
+		}
+		setIsReady(true);
+	}, []);
+};
