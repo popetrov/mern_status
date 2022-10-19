441460237ef4c09b768a32dd621fa963a8875fc7
popetrov <popetrov64@gmail.com>
2022-10-19 13:17:00 +0400

    added heroku cli

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.route'));
